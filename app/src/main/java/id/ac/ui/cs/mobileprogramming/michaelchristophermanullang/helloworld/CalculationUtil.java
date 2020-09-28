4eacf6e8f089ee1167f272e7bc16577d21f7e45d
Michael Christopher Manullang <manullang.c@gmail.com>
2020-09-28 20:25:35 +0700

    [FEATURE] Finished Lab 2 Assignment

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelchristophermanullang/helloworld/CalculationUtil.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.mobileprogramming.michaelchristophermanullang.helloworld;
+
+public class CalculationUtil {
+    public static int incrementNumber(int currentNumber){
+        return currentNumber + 1;
+    }
+}
